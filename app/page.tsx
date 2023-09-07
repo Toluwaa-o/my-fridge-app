eda7ebb262e9f60e46e06f1b17040535c8d5935f
Toluwalashe <asheogunleye02@gmail.com>
2023-09-07 03:18:42 +0100

    search bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,91 @@
 export default function Home() {
   return (
     <div className="grid min-h-screen max-w-full grid-rows-myColTemp">
-      <header className="bg-[url(https://images.unsplash.com/photo-1495195134817-aeb325a55b65?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1476&q=80)] bg-no-repeat bg-cover grid">
+      <header className="bg-[url(https://images.unsplash.com/photo-1495195134817-aeb325a55b65?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1476&q=80)] bg-no-repeat bg-cover grid p-3">
         <span className="bg-white w-[85vw] h-[6.5vh] m-auto rounded-md grid grid-cols-barTemp items-center overflow-hidden">
-          <input type="text" name="ingredient" placeholder="Add ingredient......" className="placeholder:text-sm placeholder:text-[#f78da0] placeholder:tracking-wide outline-none px-2 text-black h-[100%]" />
-          <p className="bg-[#EF4867] ml-auto uppercase text-xs font-bold tracking-wider p-2 h-[100%] w-[100%] grid place-content-center">Add</p>
+          <input
+            type="text"
+            name="ingredient"
+            placeholder="Add ingredient......"
+            className="placeholder:text-sm placeholder:text-[#f78da090] placeholder:tracking-wide outline-none px-2 text-black h-[100%]"
+          />
+          <p className="bg-[#EF4867] ml-auto uppercase text-xs font-bold tracking-wider p-2 h-[100%] w-[100%] grid place-content-center">
+            Add
+          </p>
         </span>
 
-        <span>
-          <span>
-            <p>Rice</p>
+        <span className="flex gap-3 bg-white text-gray-500 h-fit p-3 rounded-lg flex-wrap">
+          <span className="flex h-[1.5em] items-center text-center fill-white rounded overflow-hidden">
+          <span className="flex h-[1.5em] items-center text-center fill-white rounded overflow-hidden">
+            <p className="bg-[#f78da060] text-[#EF4867] uppercase text-sm font-bold h-[100%] flex items-center px-1">Rice</p>
+            <svg
+              className="h-[1.5em] w-[1.5em] bg-[#EF4867]"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+            >
+              <path d="m16.192 6.344-4.243 4.242-4.242-4.242-1.414 1.414L10.535 12l-4.242 4.242 1.414 1.414 4.242-4.242 4.243 4.242 1.414-1.414L13.364 12l4.242-4.242z"></path>
+            </svg>
+          </span>
+          <span className="flex h-[1.5em] items-center text-center fill-white rounded overflow-hidden">
+            <p className="bg-[#f78da060] text-[#EF4867] uppercase text-sm font-bold h-[100%] flex items-center px-1">Rice</p>
+            <svg
+              className="h-[1.5em] w-[1.5em] bg-[#EF4867]"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+            >
+              <path d="m16.192 6.344-4.243 4.242-4.242-4.242-1.414 1.414L10.535 12l-4.242 4.242 1.414 1.414 4.242-4.242 4.243 4.242 1.414-1.414L13.364 12l4.242-4.242z"></path>
+            </svg>
+          </span>
+            <p className="bg-[#f78da060] text-[#EF4867] uppercase text-sm font-bold h-[100%] flex items-center px-1">Rice</p>
+            <svg
+              className="h-[1.5em] w-[1.5em] bg-[#EF4867]"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+            >
+              <path d="m16.192 6.344-4.243 4.242-4.242-4.242-1.414 1.414L10.535 12l-4.242 4.242 1.414 1.414 4.242-4.242 4.243 4.242 1.414-1.414L13.364 12l4.242-4.242z"></path>
+            </svg>
+          </span>
+          <span className="flex h-[1.5em] items-center text-center fill-white rounded overflow-hidden">
+            <p className="bg-[#f78da060] text-[#EF4867] uppercase text-sm font-bold h-[100%] flex items-center px-1">Rice</p>
+            <svg
+              className="h-[1.5em] w-[1.5em] bg-[#EF4867]"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+            >
+              <path d="m16.192 6.344-4.243 4.242-4.242-4.242-1.414 1.414L10.535 12l-4.242 4.242 1.414 1.414 4.242-4.242 4.243 4.242 1.414-1.414L13.364 12l4.242-4.242z"></path>
+            </svg>
+          </span>
+          <span className="flex h-[1.5em] items-center text-center fill-white rounded overflow-hidden">
+            <p className="bg-[#f78da060] text-[#EF4867] uppercase text-sm font-bold h-[100%] flex items-center px-1">Rice</p>
+            <svg
+              className="h-[1.5em] w-[1.5em] bg-[#EF4867]"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+            >
+              <path d="m16.192 6.344-4.243 4.242-4.242-4.242-1.414 1.414L10.535 12l-4.242 4.242 1.414 1.414 4.242-4.242 4.243 4.242 1.414-1.414L13.364 12l4.242-4.242z"></path>
+            </svg>
+          </span>
+          <span className="flex h-[1.5em] items-center text-center fill-white rounded overflow-hidden">
+            <p className="bg-[#f78da060] text-[#EF4867] uppercase text-sm font-bold h-[100%] flex items-center px-1">Rice</p>
+            <svg
+              className="h-[1.5em] w-[1.5em] bg-[#EF4867]"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+            >
+              <path d="m16.192 6.344-4.243 4.242-4.242-4.242-1.414 1.414L10.535 12l-4.242 4.242 1.414 1.414 4.242-4.242 4.243 4.242 1.414-1.414L13.364 12l4.242-4.242z"></path>
+            </svg>
           </span>
         </span>
       </header>
